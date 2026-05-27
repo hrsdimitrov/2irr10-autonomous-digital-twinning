f98593e76458ab2662b8af1dae15291849757490
hrsdimitrov <hristiyan.dimitrov6dm5@gmail.com>
2026-05-27 05:48:27 +0200

    feat: added zones config file

--- src/nitrobot_mediator/setup.py
+++ src/nitrobot_mediator/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import find_packages, setup
 
 package_name = "nitrobot_mediator"
@@ -10,6 +13,7 @@ setup(
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
         ("share/" + package_name + "/launch", ["launch/mediator.launch.py"]),
+        (os.path.join("share", package_name, "config"), glob("config/*.yaml")),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
